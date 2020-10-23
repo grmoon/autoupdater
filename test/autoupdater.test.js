4785ee7bfbce4f0c4cd40123783d935c732d4a2d
Gregory Moon <gregory.r.moon@gmail.com>
2020-10-22 22:46:43 -0400

    add tests

--- test/autoupdater.test.js
+++ test/autoupdater.test.js
@@ -1,32 +1,43 @@
 const autoupdater = require("../src/autoupdater");
-const github = {
-  context: {
-    repo: jest.mock(),
-  },
-  getOctokit: jest.fn().mockReturnValue({
+
+function createOctokitMock({ prs = [] }) {
+  return {
     pulls: {
       updateBranch: jest.fn(),
-      list: jest.fn().mockReturnValue(
-        new Promise((resolve, reject) =>
-          resolve({
-            data: [
-              {
-                labels: [jest.mock()],
-              },
-            ],
-          })
-        )
-      ),
+      list: jest
+        .fn()
+        .mockReturnValue(new Promise((resolve) => resolve({ data: prs }))),
     },
-  }),
-};
+  };
+}
+
+function createGithubMock({ octokit, context } = {}) {
+  const contextMock = context || {
+    repo: jest.mock(),
+  };
+
+  const octokitMock = octokit || {
+    pulls: {
+      updateBranch: jest.fn(),
+      list: jest
+        .fn()
+        .mockReturnValue(new Promise((resolve) => resolve({ data: [] }))),
+    },
+  };
+
+  return {
+    context: contextMock,
+    getOctokit: jest.fn().mockReturnValue(octokitMock),
+  };
+}
 
 describe("autoupdater", () => {
   it("should throw an error if the GITHUB_TOKEN is falsey", async () => {
+    const githubMock = createGithubMock();
     let error;
 
     try {
-      await autoupdater(github);
+      await autoupdater(githubMock);
     } catch (e) {
       error = e;
     }
@@ -34,7 +45,48 @@ describe("autoupdater", () => {
     expect(error).toEqual(new Error("GITHUB_TOKEN must be set"));
   });
 
-  it("should work", async () => {
-    await autoupdater(github, { GITHUB_TOKEN: "GITHUB_TOKEN" });
+  it("should make the octokit call to update the correct prs", async () => {
+    const repo = { repo: "repo", owner: "owner" };
+    const context = { repo, ref: "ref" };
+    const prs = [
+      {
+        labels: [{ name: "autoupdate" }],
+        number: 0,
+      },
+      {
+        labels: [],
+        number: 1,
+      },
+      {
+        labels: [{ name: "autoupdate" }, { name: "other" }],
+        number: 2,
+      },
+      {
+        labels: [{ name: "other" }],
+        number: 3,
+      },
+    ];
+
+    const octokitMock = createOctokitMock({ prs });
+    const githubMock = createGithubMock({ context, octokit: octokitMock });
+
+    await autoupdater(githubMock, { GITHUB_TOKEN: "GITHUB_TOKEN" });
+
+    expect(octokitMock.pulls.list).toHaveBeenCalledTimes(1);
+    expect(octokitMock.pulls.list).toHaveBeenCalledWith({
+      ...repo,
+      base: "ref",
+      state: "open",
+    });
+
+    expect(octokitMock.pulls.updateBranch).toHaveBeenCalledTimes(2);
+    expect(octokitMock.pulls.updateBranch).toHaveBeenCalledWith({
+      ...repo,
+      pull_number: 0,
+    });
+    expect(octokitMock.pulls.updateBranch).toHaveBeenCalledWith({
+      ...repo,
+      pull_number: 2,
+    });
   });
 });
