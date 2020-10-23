47143f5e255a835d1efa7ea5e5a5f6ce4d393289
Gregory Moon <gregory.r.moon@gmail.com>
2020-10-22 21:42:29 -0400

    update branches

--- src/autoupdater.js
+++ src/autoupdater.js
@@ -5,24 +5,22 @@ module.exports = async function autoupdater(_github, { GITHUB_TOKEN } = {}) {
     throw new Error("GITHUB_TOKEN must be set");
   }
 
-  console.log(GITHUB_TOKEN);
-  console.log(1);
   const repo = github.context.repo;
-  console.log(2);
   const octokit = github.getOctokit(GITHUB_TOKEN);
-  console.log(3);
-  console.log(repo.repo);
-  console.log(repo);
-  const pullRequests = await octokit.pulls.list({ ...repo, state: "open" });
-  console.log(4);
+  const response = await octokit.pulls.list({ ...repo, state: "open" });
+  const pullRequests = response.data;
 
-  pullRequests.data.forEach((pullRequest) => {
-    console.log(5);
-    console.log(pullRequest);
-    console.log(6);
-    console.log("labels", pullRequest.labels);
-    console.log(7);
-    console.log("base", pullRequest.base);
-    console.log(8);
+  const promises = pullRequests.map(async (pullRequest) => {
+    if (!label.includes("autoupdate")) {
+      // continue
+      return false;
+    }
+
+    octokit.pulls.updateBranch({
+      ...repo,
+      pull_number: pullRequest.number,
+    });
   });
+
+  await Promise.all(promises);
 };
