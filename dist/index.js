3a7a478019c5f12dfcbf14f44525feaf5c058cec
Gregory Moon <gregory.r.moon@gmail.com>
2020-10-22 21:18:20 -0400

    build

--- dist/index.js
+++ dist/index.js
@@ -5795,16 +5795,22 @@ module.exports = async function autoupdater(_github, { GITHUB_TOKEN } = {}) {
     throw new Error("GITHUB_TOKEN must be set");
   }
 
+  console.log(1);
   const repo = github.context.repo;
+  console.log(2);
   const octokit = github.getOctokit(GITHUB_TOKEN);
+  console.log(3);
   const pullRequests = await octokit.pulls.list({ repo: repo.repo });
-
-  console.log(pullRequests.url);
+  console.log(4);
 
   pullRequests.data.forEach((pullRequest) => {
+    console.log(5);
     console.log(pullRequest);
+    console.log(6);
     console.log("labels", pullRequest.labels);
+    console.log(7);
     console.log("base", pullRequest.base);
+    console.log(8);
   });
 };
 
