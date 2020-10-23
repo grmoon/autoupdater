03dd9d3bfdada0212aa1f51ccdb738f8c4c3a9ee
Gregory Moon <gregory.r.moon@gmail.com>
2020-10-22 20:54:51 -0400

    update yml

--- src/autoupdater.js
+++ src/autoupdater.js
@@ -1,9 +1,8 @@
-const core = require("@actions/core");
 const github = require("@actions/github");
 
 module.exports = function autoupdater({ GITHUB_TOKEN } = {}) {
   if (!GITHUB_TOKEN) {
-    throw new Error("GITHUB_TOKEN must be set.");
+    throw new Error("GITHUB_TOKEN must be set");
   }
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
--- test/autoupdater.test.js
+++ test/autoupdater.test.js
@@ -4,6 +4,6 @@ describe("autoupdater", () => {
   it("should throw an error if the GITHUB_TOKEN is falsey", () => {
     expect(() => {
       autoupdater();
-    }).toThrow("GITHUB_TOKEN must be set.");
+    }).toThrow("GITHUB_TOKEN must be set");
   });
 });
